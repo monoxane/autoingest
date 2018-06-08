7c42793549f08d4454265d6a12aa23f5ae67110a
Oliver Herrmann <oliver@monoxane.com>
2018-06-08 11:36:48 +1000

    Day One / Base, not really functional

---
+++ logicIngest.js
@@ -0,0 +1,10 @@
+// Actual code things
+global.appver = "0.1.0"
+const fs = require('fs');
+
+// Define drive letters
+
+global.driveCF = "I:"
+global.driveSD = "J:"
+global.watchDriveSD = false
+global.systemBaseline = {}
